592a0d679577ea1c9d6e90bc8d9be043c86b8d64
pyzlnar@Yuki <pyzlnar@gmail.com>
2023-03-21 11:46:26 -0600

    :tada: Initial Commit

---
+++ lib/ueberauth_hubspot.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthHubspot do
+  @moduledoc """
+  Documentation for `UeberauthHubspot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthHubspot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_hubspot_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthHubspotTest do
+  use ExUnit.Case
+  doctest UeberauthHubspot
+
+  test "greets the world" do
+    assert UeberauthHubspot.hello() == :world
+  end
+end
